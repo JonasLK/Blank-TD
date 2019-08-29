ab1eadc248421e640bb149c0cc9a98b3850f201d
Raefion-TJ <timjansen020419@gmail.com>
2019-08-29 15:43:05 +0200

    targeting update
    test

---
+++ Blank TD/Assets/Scripts/Targeting scripts/Targeting.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeting : MonoBehaviour
+{
+    public Transform target;
+    public float range = 20f;
+
+    public string enemyTag = "Enemy";
+
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float enemyDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (enemyDistance < shortestDistance)
+            {
+                shortestDistance = enemyDistance;
+                nearestEnemy = enemy;
+            }
+        if (nearestEnemy != null && shortestDistance <= range)
+            {
+                target = nearestEnemy.transform;
+            }
+            
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+}
