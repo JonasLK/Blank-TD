f6fb8bf57d5fe9c4d25d3546ef2b8a13f090607a
Raefion-TJ <timjansen020419@gmail.com>
2019-09-25 15:40:31 +0200

    Menu, startgame

---
+++ Blank TD/Assets/Maps/Tim/UI/Code/EscapeMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeMenu : MonoBehaviour
+{
+
+    public bool EscapeOpen;
+    public GameObject Escape;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (EscapeOpen == false)
+            {
+                Escape.SetActive(true);
+                EscapeOpen = true;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Escape.SetActive(false);
+                EscapeOpen = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+}
