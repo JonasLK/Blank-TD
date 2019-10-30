6605b72ace6cf9801b9c15232e08b7b7c1d3b1a0
JonasLK <49680607+JonasLK@users.noreply.github.com>
2019-10-30 09:21:38 +0100

    golems

---
+++ Blank TD/Assets/Scripts/DealBaseDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealBaseDamage : MonoBehaviour
+{
+    public float damage;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision c)
+    {
+        if(c.gameObject.tag == "Base")
+        {
+            c.gameObject.GetComponent<BaseHealth>().Damage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
