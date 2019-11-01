4e0edba9b3a54d95d532603777afcd3c779cc493
Raefion-TJ <timjansen020419@gmail.com>
2019-11-01 12:58:02 +0100

    towermanager and lvl select

---
+++ Blank TD/Assets/Maps/Tim/UI/Code/StartGame1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame1 : MonoBehaviour
+{
+    public void ChangeMenuScene1(string sceneName)
+    {
+        SceneManager.LoadScene("Level2");
+    }
+}
