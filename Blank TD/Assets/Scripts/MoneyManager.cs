3eceb2758c032f14f8477d7b257e3963a7d109ee
JonasLK <49680607+JonasLK@users.noreply.github.com>
2019-11-01 16:11:18 +0100

    money on death

--- Blank TD/Assets/Scripts/MoneyManager.cs
+++ Blank TD/Assets/Scripts/MoneyManager.cs
@@ -11,7 +11,7 @@ public class MoneyManager : MonoBehaviour
     public Text moneyDisplay;
     public Text sellButtonDisplay;
     public Text buyPriceDisplay;
-    public float money = 100;
+    public int money;
     public float sellPrice;
     public int buyPrice;
     public bool noFakeStart;
